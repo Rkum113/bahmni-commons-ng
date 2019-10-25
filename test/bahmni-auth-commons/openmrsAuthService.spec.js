7524c8ed394cbd4f8c60204f08f30a7e1ce74bcc
MD Dubey <mddubey409@gmail.com>
2019-10-25 08:33:16 +0530

    Dubey | AP-8| Fix Tests

--- test/bahmni-auth-commons/openmrsAuthService.spec.js
+++ test/bahmni-auth-commons/openmrsAuthService.spec.js
@@ -1,21 +1,23 @@
 'use strict';
 
 describe("OpenMRSAuthService", function () {
-    var sessionService, openMRSAuthService, $bahmniCookieStore, $q, rootScope;
+    var sessionService, openMRSAuthService, $bahmniCookieStore, $q, $translate;
 
     beforeEach(module('authentication'));
     beforeEach(module(function ($provide) {
         $bahmniCookieStore = jasmine.createSpyObj('$bahmniCookieStore', ['get', 'put', 'remove']);
+        $translate = jasmine.createSpyObj('$translate', ['use']);
         $q = jasmine.createSpyObj('$q', ['all']);
         sessionService = jasmine.createSpyObj('sessionService', ['get', 'fetchLoginLocation']);
 
         $provide.value('$bahmniCookieStore', $bahmniCookieStore);
+        $provide.value('$translate', $translate);
         $provide.value('sessionService', sessionService);
         $provide.value('$q', $q);
     }));
 
     describe("populateLoginDetails", function () {
-        it("should set username and login location in cookies", inject(['sessionService', '$rootScope', 'openMRSAuthService', function (sessionService, $rootScope, openMRSAuthService) {
+        it("should set username, locale and login location in cookies", inject(['sessionService', '$rootScope', 'openMRSAuthService', function (sessionService, $rootScope, openMRSAuthService) {
             function promiseFor(responseData) {
                 return {
                     then: function (callback) {
@@ -24,7 +26,7 @@ describe("OpenMRSAuthService", function () {
                 };
             }
 
-            var fakeSessionPromise = promiseFor({data: {authenticated: true, user: {username: 'abc'}}});
+            var fakeSessionPromise = promiseFor({data: {authenticated: true, locale:'en_GB', user: {username: 'abc'}}});
             var fakeLoginLocationPromise = promiseFor({data: {authenticated: true, sessionLocation: {display: 'abc', uuid: '123'}}});
             sessionService.get.and.returnValue(fakeSessionPromise);
             sessionService.fetchLoginLocation.and.returnValue(fakeLoginLocationPromise);
@@ -36,6 +38,7 @@ describe("OpenMRSAuthService", function () {
 
             expect($bahmniCookieStore.put).toHaveBeenCalledWith('bahmni.user', 'abc', {path: '/', expires: 7});
             expect($bahmniCookieStore.put).toHaveBeenCalledWith('bahmni.user.location', {name:'abc', uuid:'123'}, {path: '/', expires: 7});
+            expect($translate.use).toHaveBeenCalledWith('en_GB');
         }]));
 
         it("should set error when not authenticated", inject(['sessionService', '$rootScope', 'openMRSAuthService', function (sessionService, $rootScope, openMRSAuthService) {
